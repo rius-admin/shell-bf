d19c9331d97418f70dad10183beb168fc22a726d
rius-admin <158822649+rius-admin@users.noreply.github.com>
2025-02-25 18:52:13 +0700

    Update shell-bf.py

--- shell-bf.py
+++ shell-bf.py
@@ -25,8 +25,9 @@ def banner():
          ||    # |  --------------------      # / 
           \\\    \=============================/  
           // 
+          """) 
           
-       {Fore.YELLOW}contoh : https://cybersederhanateam.id {Style.RESET_ALL} """)
+       {Fore.YELLOW}contoh : https://cybersederhanateam.id {Style.RESET_ALL}
 
 def check_url(url):
     """Memeriksa apakah URL memberikan status 200."""
